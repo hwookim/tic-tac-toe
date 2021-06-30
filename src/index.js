0e71681d0132ffeaf4f9f71c59f494f2132c9955
hwwokim <gusdn9754@gmail.com>
2021-06-30 17:33:46 +0900

    feat: state.squares를 통해 현재 클릭 상태 관리
     - data-no -> data-position으로 변경해 좌표값 표현

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 const App = () => {
   const state = {
     turn: "O",
+    squares: [[], [], []],
   };
 
+  const items = document.querySelectorAll(".grid-item");
+
   const init = () => {
-    const items = document.querySelectorAll(".grid-item");
     items.forEach((item) => item.addEventListener("click", onClickItem));
+    initState();
+  };
+
+  const initState = () => {
+    state.squares = [[], [], []];
+    items.forEach((item) => (item.innerText = " "));
   };
 
   const onClickItem = ({ target }) => {
     if (target.innerText) {
       return;
     }
+    const [x, y] = target.dataset.position.split(",");
 
     target.innerText = state.turn;
+    state.squares[x][y] = state.turn;
     changeTurn();
   };
 
