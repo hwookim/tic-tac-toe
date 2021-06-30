02244e302b707c9115e906c8102c675120360547
hwwokim <gusdn9754@gmail.com>
2021-06-30 14:47:28 +0900

    feat: 클릭 시 현재 턴에 따라 grid-item innertText 변경

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hi");
+const init = () => {
+  let turn = "O";
+  const items = document.querySelectorAll(".grid-item");
+
+  const changeTurn = () => {
+    turn = turn === "O" ? "X" : "O";
+  };
+
+  const onClickItem = ({ target }) => {
+    if (target.innerText) {
+      return;
+    }
+    target.innerText = turn;
+    changeTurn();
+  };
+
+  items.forEach((item) => item.addEventListener("click", onClickItem));
+};
+
+init();
